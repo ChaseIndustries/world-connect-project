72d3e8bc0afbaaaec5798d79e9eb9ef6d88bfbc2
Jordan Chase <chase@chaseu.com>
2015-05-14 23:57:43 -0400

    new css updates

---
+++ all/themes/wcp/templates/ds-1col--user-person.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 1 column template.
+ */
+?>
+<<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col person <?php print $classes;?> clearfix">
+    <img class="person__loader" src="<?php echo drupal_get_path('theme', 'wcp') ?>/images/loader.svg" />
+    <div class="person__inner">
+      <div class="person__fields">
+        <h2 class="person__name"><?php print $user->name ?></h2>
+        <div class="person__coords">
+          <div class="person__coords__lat">
+            <?php print $coords["lat"] ?>,
+          </div>
+          <div class="person__coords__lng">
+            <?php print $coords["lng"] ?>
+          </div>
+        </div>
+        <?php print $ds_content; ?>
+      </div>
+    </div>
+      <div class="person__body">
+      <img type="image/svg" src="svg/user/<?=$user->uid?>" class="person__svg" width="100%" height="100%" /></div>
+</<?php print $ds_content_wrapper ?>>
